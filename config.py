a89c6a401af6fda536e61ae54add693eb80cd536
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-21 17:20:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,19 +14,19 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "HaYa")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "soha")
 
 OWNER_ID = list(
-  map(int, getenv("OWNER_ID", "").split())) + [5369501919]
+  map(int, getenv("OWNER_ID", "").split())) + [6228635168]
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ma7amad2000/HAYA1")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/asaaqa/musek3")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HL_BG")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/HL_BG")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Mlze1bot")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Mlze1bot")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
